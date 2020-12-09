da216d37ffecf6432832ab8faf357c326576b98d
Meightee <anastasiamelnikv@gmail.com>
2020-12-09 22:13:06 +0300

    Добавьте файлы проекта.

---
+++ MoviesApp/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MoviesApp.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+    }
+}
